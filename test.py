55400d46ebbda8567526febaaf9214d1c769681e
Zyj <zyj_bupt@sina.com>
2019-11-04 23:28:13 +0800

    delete SHAN_dict.pkl

--- test.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.init as init
-import torch.nn.functional as F
-import heapq
-
-shan = torch.load('SHAN.pkl')
-print(shan)
-torch.save(shan.state_dict(), 'SHAN_dict.pkl')
